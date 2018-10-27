29bbdc57c7a9afb60532bb30cd668b11d45878d8
Gabriel <salut.gaby@gmail.com>
2018-10-27 22:44:17 +0200

    J'ajoute un texte via une directive v-bind dans l'attribut title

--- v-bind/js/index.js
+++ v-bind/js/index.js
@@ -1,8 +1,6 @@
 var app = new Vue({ // Je créer une variable étant ma nouvelle instance nommée vue comprenant une option.
     el: '#app', // je cible l'ID de l'élément HTML 
     data: { // je précise que c'est un data
-      message: 'Hello Vue !' // ce tag "message" est dans l'élément #app {{ message }} dans l'HTML et sera allimenté du string définie.
+      message: "Vous avez affiché la page à : " + new Date().toLocaleString()
     }
   });
-
-  app.message = "J'ai changé le message!"; // une fois mon objet créer, je modifie les valeurs de sa propriété ainsi.
