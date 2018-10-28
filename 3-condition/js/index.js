b9541286f2f04955e184ea5a88b056b78807f060
Gabriel <salut.gaby@gmail.com>
2018-10-28 08:52:02 +0100

    modifications orthographique

---
+++ 3-condition/js/index.js
@@ -0,0 +1,8 @@
+var app = new Vue({ // Je créer une variable étant ma nouvelle instance nommée vue comprenant une option.
+    el: '#app', // je cible l'ID de l'élément HTML 
+    data: { // je précise que c'est un data
+    visibleOuPas: true
+    }
+  });
+
+  // app.visibleOuPas = false pour cacher
