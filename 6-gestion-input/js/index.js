8ff9a446d3358c4d5b68946c0eb82f238a4e7171
Gabriel <salut.gaby@gmail.com>
2018-10-28 14:16:00 +0100

    le v-model pour un input

---
+++ 6-gestion-input/js/index.js
@@ -0,0 +1,6 @@
+var app = new Vue({ // Je créer une variable étant ma nouvelle instance nommée vue comprenant une option.
+  el: '#app', // je cible l'ID de l'élément HTML 
+  data: { // je précise que c'est un data
+    message:"hello world!"
+  }
+})
