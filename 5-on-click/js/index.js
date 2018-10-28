ba715e77c896a1497eec5b1dc924a9af6be5e763
Gabriel <salut.gaby@gmail.com>
2018-10-28 14:09:58 +0100

    on click v-on:click

---
+++ 5-on-click/js/index.js
@@ -0,0 +1,11 @@
+var app = new Vue({ // Je créer une variable étant ma nouvelle instance nommée vue comprenant une option.
+  el: '#app', // je cible l'ID de l'élément HTML 
+  data: { // je précise que c'est un data
+    message:"hello world!"
+  },
+  methods:{
+    reversMessage: function (){
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
